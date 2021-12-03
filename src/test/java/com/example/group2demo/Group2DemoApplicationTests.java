dd0736c28a69a1fffcea12ae65c67566e2210b1e
langTH0707 <langtenghuang@gmail.com>
2021-12-03 20:59:53 +0800

    Initial commit

---
+++ src/test/java/com/example/group2demo/Group2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.group2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group2DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
